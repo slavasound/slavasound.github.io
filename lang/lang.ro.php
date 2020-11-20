5236b17e305ec6b9630cb6b5deaf721c20b9e222
slavasound <44759886+slavasound@users.noreply.github.com>
2020-11-20 23:19:31 +1100

    Create lang.ro.php

---
+++ lang/lang.ro.php
@@ -0,0 +1,18 @@
+<?php
+
+/** ----------------------
+ ** Румынский языковый файл
+ ** Источник: live-code.ru
+ ** ----------------------*/
+$Lang = array(
+ "title" 	=> "Site-ul meu multilingvist pe PHP",
+ "header_title" => "Site-ul poliglot",
+ "site_slogan"	=> "Site-ul programistului",
+ 
+ // Меню
+ "index_menu"	=> "Pagina principala",
+ "contact_menu" => "Contacte",
+ "site_map"	=> "Mapa Site-ului",
+ "advertisement"=> "Reclama",
+);
+?>
